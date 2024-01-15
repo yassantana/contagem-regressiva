9be0cd06a4acc0429876906667593c677862ac47
Yasmin Santana <103133345+yassantana@users.noreply.github.com>
2024-01-15 15:59:20 -0300

    Nova versão com ajustes
    Ajustes no formato da descrição das datas, simplificando as funções para não ficarem tão extensas, assim fica um código limpo e fácil de compreender.

--- app.js
+++ app.js
@@ -3,9 +3,24 @@ const secondsContainer = document.querySelector('#seconds')
 const minutesContainer = document.querySelector('#minutes')
 const hoursContainer = document.querySelector('#hours')
 const daysContainer = document.querySelector('#days')
+const nextYearContainer = document.querySelector('#year')
+const spinnerLoading = document.querySelector('#loading')
+const countdownContainer = document.querySelector('#countdown')
 
-const nextYear = new Date().getFullYear() + 1
+const nextYear = new Date().getFullYear() +1
 const newYearTime = new Date (`January 01 ${nextYear} 00:00:00`)
+
+nextYearContainer.textContent = nextYear
+
+const getTimeUnit = unit => unit < 10 ? '0' + unit : unit
+
+const insertCountdownValues = ({ days, hours, minutes, seconds }) => {
+secondsContainer.textContent = getTimeUnit(seconds)
+minutesContainer.textContent = getTimeUnit(minutes)
+hoursContainer.textContent = getTimeUnit(hours)
+daysContainer.textContent = getTimeUnit(days)
+
+}
 
 const updateCountdown = () => {
     const currentTime = new Date()
@@ -15,14 +30,15 @@ const updateCountdown = () => {
         const minutes = Math.floor(difference / 1000 / 60) % 60
         const seconds = Math.floor(difference / 1000) % 60
 
-        secondsContainer.textContent = seconds < 10 ? '0' + seconds : seconds
-        minutesContainer.textContent = minutes < 10 ? '0' + minutes : minutes
-        hoursContainer.textContent = hours < 10 ? '0' + hours : hours
-        daysContainer.textContent = days < 10 ? '0' + days : days
-
-    
+    insertCountdownValues ({ days, hours, minutes, seconds })
 
 }
-console.log(secondsContainer)
+
+const handleCountdownDisplay = () => {
+    spinnerLoading.remove()
+    countdownContainer.style.display = 'flex'
+  }
+
+setTimeout(handleCountdownDisplay, 1000)
 
 setInterval(updateCountdown, 1000)
